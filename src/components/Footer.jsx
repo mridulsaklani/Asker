d134b37342d23b7c82fa5cb7185a8fe613611abc
MRIDULSAKLANI <saklanimridul@gmail.com>
2025-06-03 07:58:19 +0530

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <>
       <div className='w-full bg-blue-600 py-4 fixed bottom-0'>
         <div className="max-w-7xl mx-auto flex justify-center">
-           <p className='text-white font-medium tracking-wide'> &copy; {date} | by: <Link to={'https://mridulsinghsaklani.com'} target='_blank' className='underline font-normal hover:scale-101 transition-all duration-300'> mridulsinghsaklani.com</Link>    </p>
+           <p className='text-white font-medium tracking-wide'> &copy; {date} | Developed by: <Link to={'https://mridulsinghsaklani.com'} target='_blank' className='underline font-normal hover:scale-101 transition-all duration-300'> mridulsinghsaklani.com</Link>    </p>
         </div>
 
       </div>
